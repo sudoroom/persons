4c28136c0beac54549fd05564f5c0d2613df5260
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-14 00:19:20 -0700

    Display person e-mail addresses and keys

---
+++ routes/person.js
@@ -0,0 +1,61 @@
+var doNotCache = require('do-not-cache')
+var internalError = require('./internal-error')
+var readPerson = require('../data/read/person')
+
+var escape = require('./escape')
+var footer = require('./partials/footer')
+var head = require('./partials/head')
+var header = require('./partials/header')
+var html = require('./html')
+var nav = require('./partials/nav')
+
+module.exports = function (request, response, settings) {
+  var personID = request.parameters.person
+  readPerson(settings, personID, function (error, person) {
+    /* istanbul ignore if */
+    if (error) return internalError(response, error, settings)
+    var data = {person: person}
+    settings.log.info(data, 'data')
+    render(data)
+  })
+
+  function render (data) {
+    doNotCache(response)
+    response.setHeader('Content-Type', 'text/html; charset=UTF-8')
+    response.end(html`
+<!doctype html>
+<html lang=EN>
+${head()}
+<body>
+  ${nav()}
+  ${header()}
+  <main>
+    <h2>${escape(data.person.name)}</h2>
+    ${data.keys.length !== 0 && html`
+    <section>
+      <h3>E-Mail</h3>
+      <p>
+        <a href="mailto:${escape(data.email)}">
+          ${escape(data.email)}
+        </a>
+      </p>
+    </section>
+    <section>
+      <h3>Keys</h3>
+      <dl>
+        ${data.keys.map(function (key) {
+          return html`
+            <dt>${escape(key.type)}</dt>
+            <ddt>${escape(key.key)}</dd>
+          `
+        })}
+      </dt>
+    </section>
+    `}
+  </main>
+  ${footer()}
+</body>
+</html>
+  `)
+  }
+}
--- test/create-test-data.js
+++ test/create-test-data.js
@@ -36,9 +36,30 @@ module.exports = function (settings, callback) {
           fs.writeFile(file, JSON.stringify({
             id: personID,
             name: 'John Doe',
+            email: 'john@example.com',
             memberships: [
               {
                 collective: collectiveID,
+                keys: [
+                  {
+                    type: 'SSH',
+                    key: [
+                      'ssh-rsa AAAAB3NzaC1yc2EAAAADAQAB' +
+                      'AAABAQDBuW5UoEIU/xo31NIwFbY2QVin' +
+                      'Nxxz5po0UO7pBp8sxDcsunjiMKVMFC34' +
+                      'E3IPcsJWmdQNnOVqp+/gVvc921Olckhx' +
+                      'AQroPev6tyE/S8it5y1YRXSZk9v6Tqif' +
+                      'xoE2/2TZm2DHZEfVt3tmcjbxnknPNokp' +
+                      'n/XNygg2Vi2CvVMoXwn4MVuRn7r+y41T' +
+                      'viSnGrHTq7oXIJfiWUu1/JYb0ehPOQ4/' +
+                      'z5fiAe1MpN0arxu2JoMqAE2yUfAX9IIs' +
+                      'o9T45PHjCJmsTJ20TPwziVLDGIhK1fvb' +
+                      '3HeLwDsM1VD3jh5GJeJA+Ii6awA4exYl' +
+                      '0ovPhsaCteZgpSB+BfHZfp9++66V ' +
+                      'test@example.com'
+                    ].join()
+                  }
+                ],
                 date: new Date().toISOString()
               }
             ]
